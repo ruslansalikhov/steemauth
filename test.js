73fa3f0ce022845dc29192ecb8239f2290f11f00
adcpm <fabien@adcpm.com>
2016-09-03 19:14:32 +0700

    - Refactor

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-var steemAuth = require('./lib/steemauth');
-var steem = require('steem');
-var username = process.env.STEEM_USERNAME;
-var password = process.env.STEEM_PASSWORD;
-var pubWif = process.env.STEEM_POSTING_PUBLIC_WIF;
-var privWif = process.env.STEEM_POSTING_PRIVATE_WIF;
-var memoKey = process.env.STEEM_MEMOKEY_PUBLIC;
+var steemAuth = require('./lib/steemauth'),
+	steem = require('steem');
+
+var username = process.env.STEEM_USERNAME,
+	password = process.env.STEEM_PASSWORD,
+	pubWif = process.env.STEEM_POSTING_PUBLIC_WIF,
+	privWif = process.env.STEEM_POSTING_PRIVATE_WIF,
+	memoKey = process.env.STEEM_MEMOKEY_PUBLIC;
 
 var isValid = steemAuth.verify(username, password, { posting: [[pubWif, 1]] });
 console.log(isValid);
@@ -13,13 +14,15 @@ var isWif = steemAuth.isWif(privWif);
 console.log(isWif);
 
 var ownerWif = steemAuth.toWif(username, password, 'owner');
-console.log(ownerWif);
+
+var memoKeyWif = steemAuth.toWif(username, password, 'memokey');
 
 var wifIsValid = steemAuth.wifIsValid(privWif, pubWif);
 console.log(wifIsValid);
 
 var owner, active, posting,
-	jsonMetadata = { 'profile': { 'name': 'Steem.js', updateOn: new Date().toString() } };
+	jsonMetadata = {'profile': { 'name': 'Steem.js'}};
+
 
 steem.broadcast.accountUpdate(ownerWif, username, undefined, undefined, undefined, memoKey, jsonMetadata, function (err, result) {
 	console.log(err, result);
